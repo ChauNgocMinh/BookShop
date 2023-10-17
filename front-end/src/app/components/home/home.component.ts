f9c648feddea8143da17829a575fdca2f69b482b
MinhChau <chaungocminh202@gmail.com>
2023-10-17 11:39:20 +0700

    update home page

--- front-end/src/app/components/home/home.component.ts
+++ front-end/src/app/components/home/home.component.ts
@@ -8,3 +8,22 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
 }
+
+function show_right(): void{
+  let currentIndex = 0;
+  var items = document.querySelectorAll<HTMLElement>("#item");
+  
+  items[currentIndex].style.display = "none"; // Ẩn thẻ hiện tại
+  currentIndex = (currentIndex + 1) % items.length; // Di chuyển đến thẻ tiếp theo
+  items[currentIndex].style.display = "block"; // Hiển thị thẻ tiếp theo
+}
+// const left = document
+// const right 
+// const imageContainer = document.getElementById('item-img') as HTMLElement;
+
+// function slideImage() {
+//   imageContainer.style.top = '0';
+// }
+
+// // Gọi hàm slideImage sau khi trang đã tải
+// window.addEventListener('load', slideImage);
