59f8d980e379e93cb32c0d2c44c9057a686c38a4
MinhChau <chaungocminh202@gmail.com>
2023-10-21 00:52:22 +0700

    create products page

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
-import { NgModule } from '@angular/core';
+
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 const routes: Routes = [
-
+  { path: '', component: HomeComponent },
+  {path: "products", component: ProductsComponent}
 ];
 
 @NgModule({
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,12 +4,14 @@ import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
 @NgModule({
-  declarations: [ AppComponent, LoginComponent, HomeComponent],
-  imports: [ BrowserModule, AppRoutingModule, FormsModule ],
+  declarations: [ AppComponent, LoginComponent, HomeComponent, ProductsComponent],
+  imports: [ BrowserModule, AppRoutingModule, FormsModule, RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
