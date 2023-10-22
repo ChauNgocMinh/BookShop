91560bb7594ff297835c9c5ed42fabeb75854615
MinhChau <chaungocminh202@gmail.com>
2023-10-22 19:05:25 +0700

    update products page

--- front-end/src/app/components/products/products.component.ts
+++ front-end/src/app/components/products/products.component.ts
@@ -15,6 +15,12 @@ export class ProductsComponent {
       this.products = data;
     })
   }
+
+  isClassAdded: boolean = false;
+
+  public addClass() {
+    this.isClassAdded = !this.isClassAdded;
+  }
 }
 
 interface Product {
