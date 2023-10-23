9cc48edeb7f3750d89707b9e9f7967232579ff4a
nguyentranthanhliem221002 <nguyentranthanhliem.2002@gmail.com>
2023-10-23 23:50:35 +0700

    TEST

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './components/products/products.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  {path: "products", component: ProductsComponent}
+  {path: "products", component: ProductsComponent},
+  {path:"login", component:LoginComponent}
 ];
 
 @NgModule({
--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, getNgModuleById } from '@angular/core';
 import * as $ from "jquery";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
