ab46592467adbbb3d738c68b3dad0e3b78e36dc8
nguyentranthanhliem221002 <nguyentranthanhliem.2002@gmail.com>
2023-10-26 16:00:50 +0700

    amination-link-login-register

--- front-end/src/app/components/login/login.component.ts
+++ front-end/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
+import { keyframes } from '@angular/animations';
 import { Component } from '@angular/core';
-
+var runny;
+var imgg = document.getElementById('ig');
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
